dfc2c4a406713a6bef34a7535be2a6a97ef639b4
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-12-26 21:29:30 +0100

    add cors middleware

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -1,7 +1,34 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Configuration, OpenAIApi } from 'openai';
+import Cors from 'cors';
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   if (req.method === 'POST') {
     const configuration = new Configuration({
       apiKey: process.env.OPENAI_API_KEY,
