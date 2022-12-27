969a041f5f8c110c36fa79df5454ccc675b134e5
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-12-27 16:31:35 +0100

    adding responsiveness to the app

--- components/Home.tsx
+++ components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <main className={styles.home}>
       <section className={styles.wrapper}>
-        <h1 className={styles.gradient__text}>Welcome in the new Era of AI</h1>
+        <h1 className={styles.gradient__text}> the new Era of AI</h1>
         <Image src="/ai.png" alt="ai" width={700} height={700} priority />
         <button>
           <Link href="/chat">OpenAI Jarvis</Link>
