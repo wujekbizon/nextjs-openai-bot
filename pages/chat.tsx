4ea936d485841d18f9be56eba15405c892f6ee8c
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-12-28 08:31:57 +0100

    adding metadata

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,6 +1,15 @@
 import Chat from '../components/Chat';
+import Head from 'next/head';
 
 const ChatPage = () => {
-  return <Chat />;
+  return (
+    <>
+      <Head>
+        <title>Jarvis GPT 3 Chat Bot</title>
+        <meta name="description" content="Real Time GPT 3 Chat Bot" />
+      </Head>
+      <Chat />
+    </>
+  );
 };
 export default ChatPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
 import Home from '../components/Home';
+import Head from 'next/head';
 
 const HomePage = () => {
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <title>Welcome in The New Era Of AI</title>
+        <meta
+          name="description"
+          content="With An Elegant User Interface, Communication With Advanced GPT3 Model API, And Most Importantly, The Ability To Ask The AI For Help Regarding JavaScript, React, Or Any Other Programming Language, Giving It Code And Translating It To Another Programming Language, And Much More.. This Is Jarvis Chat Bot."
+        />
+      </Head>
+      <Home />
+    </>
+  );
 };
 export default HomePage;
